4691588b40dc387d33d567136163024076e618a0
Matej Zelenak <maejzele@gmail.com>
2024-03-25 20:51:02 +0100

    add bullets to the gun

---
+++ characters/player/bullet.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var travelled = 0
+const DAMAGE = 5
+
+func _physics_process(delta):
+	const SPEED = 1000
+	const RANGE = 1200
+	
+	var direction = Vector2.RIGHT.rotated(rotation)
+	
+	position += direction * SPEED * delta
+	travelled += SPEED * delta
+	if travelled > RANGE:
+		queue_free()
+
+
+func _on_body_entered(body):
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage(DAMAGE)
