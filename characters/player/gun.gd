07f043a9c00297a066088fa1893b8626f40012f7
Matej Zelenak <maejzele@gmail.com>
2024-03-26 16:38:17 +0100

    add the ability to aim freely

--- characters/player/gun.gd
+++ characters/player/gun.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
+const BULLET = preload("res://characters/player/bullet.tscn")
+const AUTO_AIM: bool = false
+		
 func _physics_process(delta):
-	var enemies = get_overlapping_bodies()
-	if enemies.size() > 0:
-		look_at(enemies[0].global_position)
+	if AUTO_AIM:
+		var enemies = get_overlapping_bodies()
+		if enemies.size() > 0:
+			look_at(enemies[0].global_position)
+			
+	else:
+		look_at(get_global_mouse_position())
 
 
-func shoot():
-	const BULLET = preload("res://characters/player/bullet.tscn")
+func shoot() -> void:
 	var new_bullet = BULLET.instantiate()
 	new_bullet.global_position = %ShootPoint.global_position
 	new_bullet.global_rotation = %ShootPoint.global_rotation
