f29bb000d50074d7c61009995c67ecc3874cc15a
Mich_H <hruzko@gmail.com>
2024-03-26 14:57:14 +0100

    Reworked entity _init and added resource class
    Entity now works with resources instead of multiple arguments in init. Now it is sufficient to create a resource file "name_of_entity".tres and plug that into the init of entity (_init("name_of_entity"))

---
+++ characters/entity_stats.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name EntityStats
+
+@export var current_health = 0
+@export var max_health = 0
+@export var min_health = 0
+
+@export var current_speed = 0
+@export var max_speed = 0
+@export var min_speed = 0
--- tests/unit/test_entity.gd
+++ tests/unit/test_entity.gd
@@ -1,10 +1,10 @@
 extends GutTest
 
 func test_entity_innit_all_zeros():
-	var correct_health_values = [0, 0, 0]
-	var correct_speed_values = [0, 0, 0]
+	var correct_health_values = [1, 1, 1]
+	var correct_speed_values = [2, 2, 2]
 	
-	var innitialized_entity: Entity = Entity.new(0, 0, 0, 0, 0, 0)
+	var innitialized_entity: Entity = Entity.new("test")
 	
 	assert_eq([
 		innitialized_entity.health.current_value, 
@@ -16,10 +16,10 @@ func test_entity_innit_all_zeros():
 		innitialized_entity.speed.min_value], correct_speed_values)
 
 func test_entity_innit_with_values():
-	var correct_health_values = [5, 100, 0]
-	var correct_speed_values = [5, 10, 0]
+	var correct_health_values = [100, 100, 0]
+	var correct_speed_values = [50, 50, 0]
 	
-	var innitialized_entity: Entity = Entity.new(5, 100, 0, 5, 10, 0)
+	var innitialized_entity: Entity = Entity.new("skeleton")
 	
 	assert_eq([
 		innitialized_entity.health.current_value, 
