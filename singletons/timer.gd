9c9a075be4b37810e1c3575554056ac755a88a1b
Norbert Hašan <147330838+bengnoro@users.noreply.github.com>
2024-04-02 23:16:50 +0200

    add run timer

---
+++ singletons/timer.gd
@@ -0,0 +1,19 @@
+class_name RunTimer
+extends Timer
+
+signal run_finished
+var time_elapsed:int = 0
+@export var game_lenght:int = 10 * 60
+
+func _on_timeout():
+	time_elapsed += 1
+	if time_left() <= 0:
+		_on_run_finished()
+
+
+func time_left() -> int:
+	return game_lenght - time_elapsed
+
+
+func _on_run_finished():
+		emit_signal("run_finished")
