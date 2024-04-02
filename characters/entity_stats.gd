3d630f3915b39dbb18bf4065e53bc8fb5de73364
Mich_H <hruzko@gmail.com>
2024-04-02 16:46:25 +0200

    entity attack_speed added
    Entity now has attack speed.

--- characters/entity_stats.gd
+++ characters/entity_stats.gd
@@ -8,3 +8,7 @@ class_name EntityStats
 @export var current_speed = 0
 @export var max_speed = 0
 @export var min_speed = 0
+
+@export var current_attack_speed = 1.0
+@export var max_attack_speed = 1.0
+@export var min_attack_speed = 1.0
