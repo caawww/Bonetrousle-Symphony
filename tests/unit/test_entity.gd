ad8e09dbcff5e68c668884d2d311ec7230b620c9
Mich_H <hruzko@gmail.com>
2024-04-02 17:45:47 +0200

    entity attack_speed added

--- tests/unit/test_entity.gd
+++ tests/unit/test_entity.gd
@@ -3,6 +3,7 @@ extends GutTest
 func test_entity_init_all_zeros():
 	var correct_health_values = [1.0, 1.0, 1.0]
 	var correct_speed_values = [2.0, 2.0, 2.0]
+	var correct_attack_speed_values = [1.0, 1.0, 1.0]
 	
 	var initialized_entity: Entity = Entity.new("test")
 	
@@ -14,10 +15,15 @@ func test_entity_init_all_zeros():
 		initialized_entity.speed.current_value, 
 		initialized_entity.speed.max_value, 
 		initialized_entity.speed.min_value], correct_speed_values)
+	assert_eq([
+		initialized_entity.attack_speed.current_value, 
+		initialized_entity.attack_speed.max_value, 
+		initialized_entity.attack_speed.min_value], correct_attack_speed_values)
 
 func test_entity_init_with_values():
 	var correct_health_values = [100.0, 100.0, 0.0]
 	var correct_speed_values = [50.0, 50.0, 0.0]
+	var correct_attack_speed_values = [1.0, 2.0, 1.0]
 	
 	var initialized_entity: Entity = Entity.new("skeleton")
 	
@@ -29,3 +35,7 @@ func test_entity_init_with_values():
 		initialized_entity.speed.current_value, 
 		initialized_entity.speed.max_value, 
 		initialized_entity.speed.min_value], correct_speed_values)
+	assert_eq([
+		initialized_entity.attack_speed.current_value, 
+		initialized_entity.attack_speed.max_value, 
+		initialized_entity.attack_speed.min_value], correct_attack_speed_values)
