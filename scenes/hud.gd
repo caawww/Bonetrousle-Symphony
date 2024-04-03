ac6f98dfafe3b95fa3ff7e2af60e14b0b721a618
Michal Kubirita <michal.kubirita@gmail.com>
2024-04-03 21:16:36 +0200

    show run timer in HUD

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var health_bar = %HealthBar
+@onready var run_timer = %RunTimer
 
 
 func _ready():
@@ -12,7 +13,13 @@ func _ready():
 
 
 func _process(_delta):
-	pass
+	_update_elapsed_time()
+
+
+func _update_elapsed_time() -> void:
+	var minutes_elapsed: int = run_timer.time_elapsed / 60
+	var seconds_elapsed: int = run_timer.time_elapsed % 60
+	%RunTimerLabel.text = "%02d:%02d" % [minutes_elapsed, seconds_elapsed]
 
 
 func _on_player_health_changed(value: int):
