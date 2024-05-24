88bbfcd9205f7ca6dca208100fec80c9beda3f28
Lonthy <michal.kubirita@gmail.com>
2024-05-24 21:48:15 +0200

    refactor skeleton and enemy

--- characters/enemies/enemy.gd
+++ characters/enemies/enemy.gd
@@ -4,3 +4,23 @@ extends Entity
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: Player = get_node("../Player")
+	
+
+func _on_health_changed(_value):
+	if health.current_value == 0:
+		RunStatistics.add_score(score_per_kill)
+		queue_free()
+		
+	if $AnimationPlayer.is_playing:
+		$AnimationPlayer.stop()
+
+	$AnimationPlayer.play("hit_flash")
+
+
+func deal_damage():
+	var targets = $Hurtbox.get_overlapping_bodies()
+	if targets.size() > 0 and $Timer.get_time_left() == 0:
+		for target in targets:
+			if target.has_method("take_damage"):
+				target.take_damage(1)
+				$Timer.start()
--- characters/enemies/skeleton/skeleton.gd
+++ characters/enemies/skeleton/skeleton.gd
@@ -20,23 +20,3 @@ func flip_sprite(direction: Vector2):
 			sprite.set_flip_h(true)
 
 		sprite.play("walk")
-
-
-func deal_damage():
-	var targets = $Hurtbox.get_overlapping_bodies()
-	if targets.size() > 0 and $Timer.get_time_left() == 0:
-		for target in targets:
-			if target.has_method("take_damage"):
-				target.take_damage(1)
-				$Timer.start()
-
-
-func _on_health_changed(_value):
-	if health.current_value == 0:
-		RunStatistics.add_score(score_per_kill)
-		queue_free()
-		
-	if $AnimationPlayer.is_playing:
-		$AnimationPlayer.stop()
-
-	$AnimationPlayer.play("hit_flash")
