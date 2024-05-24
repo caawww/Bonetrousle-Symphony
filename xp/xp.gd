210591922f59096ef97be0cfbfa574df46f84811
Pia669 <pia669.horvath@gmail.com>
2024-05-24 21:39:44 +0200

    enemy drop xp

--- xp/xp.gd
+++ xp/xp.gd
@@ -1,20 +1,18 @@
 class_name Xp
 extends Node2D
 
-enum XP_LEVEL {NONE, XP1}
+enum XP_LEVELS {NONE=-1, XP1}
 
-const XP_SPRITES = {XP1 = "res://sprites/xp/xp1.png"}
-const XP_VALUES = {XP1 = 1}
+const XP_PRELOAD = preload("res://xp/xp.tscn")
 
-var level: XP_LEVEL
-
-
-func _ready():
-	$Sprite2D.texture = XP_SPRITES.get(level)
+var level: XP_LEVELS
+@export var xp_sprites: Array[Texture]
+@export var xp_vlaues: Array[int]
 
 
-func set_xp_level(lvl: XP_LEVEL):
+func set_xp_level(lvl: XP_LEVELS):
 	level = lvl
+	$Sprite2D.texture = xp_sprites[lvl]
 
 
 func _on_area_2d_body_entered(body):
